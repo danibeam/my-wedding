d54d01352dc1265a768931cb1e033ab946840f89
Daniel Belmonte Amorós <dbelmonte@plainconcepts.com>
2025-06-02 17:01:26 +0200

    New build

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,7 +1,7 @@
 import { renderers } from './renderers.mjs';
 import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_Ds66R7Fl.mjs';
+import { manifest } from './manifest_CH6su7Zh.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
@@ -10,13 +10,15 @@ const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/api/confirm.astro.mjs');
 const _page2 = () => import('./pages/guest/success-confirm.astro.mjs');
 const _page3 = () => import('./pages/guest/_token_.astro.mjs');
-const _page4 = () => import('./pages/index.astro.mjs');
+const _page4 = () => import('./pages/guests-list.astro.mjs');
+const _page5 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
     ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["src/pages/api/confirm.ts", _page1],
     ["src/pages/guest/success-confirm.astro", _page2],
     ["src/pages/guest/[token].astro", _page3],
-    ["src/pages/index.astro", _page4]
+    ["src/pages/guests-list.astro", _page4],
+    ["src/pages/index.astro", _page5]
 ]);
 
 const _manifest = Object.assign(manifest, {
@@ -27,7 +29,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "3c4bfeb1-713f-4de1-b78a-cc7b604ca670"
+    "middlewareSecret": "be67b605-d36d-4cce-8639-70f35d3ca95b"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
