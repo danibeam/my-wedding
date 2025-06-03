100e091eb395cffee580191060accdb34a337e98
Daniel Belmonte Amorós <dbelmonte@plainconcepts.com>
2025-06-03 19:39:23 +0200

    Update guests list with more info

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,6 +2,6 @@ export interface Guest {
     id: string;
     name: string;
     email: string;
-    attendance: string; // '0' = not confirmed, '1' = confirmed
+    attendance: string; // '0' = not confirmed, '1' = confirmed, '-1' = declined
     token: string;
 }
