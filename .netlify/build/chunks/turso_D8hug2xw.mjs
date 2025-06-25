e5dfbfe6f11cad47fb13d3678a4779b1ba846a14
Daniel Belmonte <dbelmonte@plainconcepts.com>
2025-06-25 19:05:31 +0200

    ✨: Improvements

--- .netlify/build/chunks/turso_D8hug2xw.mjs
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@libsql/client/web';
-
-const turso = createClient({
-  url: "libsql://dani-sofi-weding-danibeam.turso.io",
-  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NDMyNzA3ODMsImlkIjoiY2RlMzE0MDAtZjFiOC00YTI0LTllOGUtMjQxZTU1N2NjMDJiIiwicmlkIjoiNjhhMDMzYTUtOGNkOS00ZjJmLWI2MDctMGNlZmYxZTU5YTI1In0.TW9kWprb4p3qpkvcMULRgJi-AlLvI1Cqo7qcj_-pwShwIvp2pn9WtHNLk-pq9KKynP0GeSQOgEwF1IJZk6nSCw"
-});
-
-export { turso as t };
